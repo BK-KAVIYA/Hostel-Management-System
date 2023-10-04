7c986cfdeb322aa8afe40e4186e28f557491a0a5
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-10-04 11:36:32 +0530

    complaint

--- db/hosteldbms_complaint.sql
+++ db/hosteldbms_complaint.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `hosteldbms` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `hosteldbms`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: hosteldbms
 -- ------------------------------------------------------
--- Server version	5.7.31
+-- Server version	8.0.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,7 +23,7 @@ DROP TABLE IF EXISTS `complaint`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `complaint` (
-  `compalint_id` int(11) NOT NULL AUTO_INCREMENT,
+  `compalint_id` int NOT NULL AUTO_INCREMENT,
   `asset_id` varchar(30) NOT NULL,
   `complaint` varchar(255) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
@@ -41,15 +39,6 @@ CREATE TABLE `complaint` (
   KEY `warden_id` (`warden_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `complaint`
---
-
-LOCK TABLES `complaint` WRITE;
-/*!40000 ALTER TABLE `complaint` DISABLE KEYS */;
-/*!40000 ALTER TABLE `complaint` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -60,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-26 17:06:04
+-- Dump completed on 2023-10-04 11:29:13
