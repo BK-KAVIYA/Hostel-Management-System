82b6ab35abcc46e34db9f12f3facebdb79e8d8f4
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-10-04 11:36:56 +0530

    hostel

--- db/hosteldbms_hostel.sql
+++ db/hosteldbms_hostel.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `hosteldbms` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `hosteldbms`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: hosteldbms
 -- ------------------------------------------------------
--- Server version	5.7.31
+-- Server version	8.0.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,23 +23,14 @@ DROP TABLE IF EXISTS `hostel`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `hostel` (
-  `h_id` int(11) NOT NULL AUTO_INCREMENT,
+  `h_id` int NOT NULL AUTO_INCREMENT,
   `hostel_name` varchar(255) NOT NULL,
-  `capacity` int(11) NOT NULL,
+  `capacity` int NOT NULL,
   `sub_warden_id` varchar(255) NOT NULL,
   PRIMARY KEY (`h_id`),
   KEY `sub_warden_id` (`sub_warden_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `hostel`
---
-
-LOCK TABLES `hostel` WRITE;
-/*!40000 ALTER TABLE `hostel` DISABLE KEYS */;
-/*!40000 ALTER TABLE `hostel` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -52,4 +41,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-26 17:06:02
+-- Dump completed on 2023-10-04 11:29:11
