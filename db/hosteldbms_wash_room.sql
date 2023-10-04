e94f4d2b91c889e865c97f71eaa1185b4580b25b
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-10-04 11:38:08 +0530

    wash_room

--- db/hosteldbms_wash_room.sql
+++ db/hosteldbms_wash_room.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `hosteldbms` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `hosteldbms`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: hosteldbms
 -- ------------------------------------------------------
--- Server version	5.7.31
+-- Server version	8.0.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,22 +23,13 @@ DROP TABLE IF EXISTS `wash_room`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `wash_room` (
-  `wash_room_id` int(11) NOT NULL AUTO_INCREMENT,
-  `floor` int(5) NOT NULL,
-  `hostel_id` int(11) NOT NULL,
+  `wash_room_id` int NOT NULL AUTO_INCREMENT,
+  `floor` int NOT NULL,
+  `hostel_id` int NOT NULL,
   PRIMARY KEY (`wash_room_id`),
   KEY `hostel_id` (`hostel_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `wash_room`
---
-
-LOCK TABLES `wash_room` WRITE;
-/*!40000 ALTER TABLE `wash_room` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wash_room` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -51,4 +40,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-26 17:06:04
+-- Dump completed on 2023-10-04 11:29:12
